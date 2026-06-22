6e71006e73fa723dcc7d56513ddea8247e72f1a2
Herschel_TC <raphael.lesterlin@gmail.com>
2026-06-22 18:14:46 +0200

    finished adding models and base creation, will add validators and other interactions later

--- PyStruct/arvi_api/main.py
+++ PyStruct/arvi_api/main.py
@@ -15,7 +15,7 @@ def root() -> dict:
     return {"status": "OK", "scope": "DO NOT USE FOR MEDICAL DIAGNOSIS, THIS IS A PROJECT FOR EDUCATIONAL PURPOSES"}
 
 @app.post("/test/", response_model=TestResponse)
-async def create_item(item: TestCreate, db: Session = Depends(get_db)):
+async def create_test(item: TestCreate, db: Session = Depends(get_db)):
     db_item = Test(**item.model_dump())
     db.add(db_item)
     db.commit()
@@ -23,7 +23,7 @@ async def create_item(item: TestCreate, db: Session = Depends(get_db)):
     return db_item
 
 @app.post("/case/", response_model=CaseResponse)
-async def create_item(item: CaseCreate, db: Session = Depends(get_db)):
+async def create_case(item: CaseCreate, db: Session = Depends(get_db)):
     try:
         db_item = Case(**item.model_dump())
         db.add(db_item)
@@ -32,3 +32,27 @@ async def create_item(item: CaseCreate, db: Session = Depends(get_db)):
         return db_item
     except ValueError as e:
         raise HTTPException(status=406, detail="Item does not match validation criterions : " + str(e))
+    
+@app.post("/prompts/", response_model=PromptsResponse)
+async def create_prompt(item: PromptsCreate, db: Session = Depends(get_db)):
+    db_item = Prompts(**item.model_dump())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+@app.post("/runs/", response_model=RunsResponse)
+async def create_run(item: RunsCreate, db: Session = Depends(get_db)):
+    db_item = Runs(**item.model_dump())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+@app.post("/eval/", response_model=EvaluationsResponse)
+async def create_eval(item: EvaluationsCreate, db: Session = Depends(get_db)):
+    db_item = Evaluations(**item.model_dump())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
--- PyStruct/arvi_api/resources/models.py
+++ PyStruct/arvi_api/resources/models.py
@@ -1,10 +1,11 @@
 """This file exists solely for the purpose of storing model classes and other Base models outside of the main file."""
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from pydantic import BaseModel, field_validator
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.types import *
 from sqlalchemy import create_engine
 from pathlib import Path
+from datetime import datetime
 
 # Database setup is done in this file, else the table models don't work
 DATABASE_URL = "sqlite:///../../sqlite/arvi_rx.db"
@@ -50,7 +51,7 @@ class CaseCreate(BaseModel):
     
     @field_validator("split")
     @classmethod
-    def validate_label(cls, value):
+    def validate_split(cls, value):
         allowed = {"train", "test"}
         if value not in allowed:
             raise ValueError(f"split must be among {allowed}")
@@ -75,6 +76,90 @@ class CaseResponse(BaseModel):
     ground_truth_label : str
     split : str
     notes : str
+
+class Prompts(Base):
+    __tablename__ = "prompts"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+    version = Column(String(50), nullable=False)
+    content = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=datetime.now())
+
+class PromptsCreate(BaseModel):
+    name : str
+    version : str
+    content : str
+
+    # Validators to insert
+
+class PromptsResponse(BaseModel):
+    id : int
+    name : str
+    version : str
+    content : str
+    created_at : datetime
+
+class Runs(Base):
+    __tablename__ = "runs"
+    id = Column(Integer, primary_key=True, index=True)
+    case_id = Column(Integer, ForeignKey(Case.id), nullable=False) # Replace redundant info for normalization purposes by foreign key references
+    prompt_id = Column(Integer, ForeignKey(Prompts.id), nullable=False)
+    model_used = Column(String(50))
+    prediction_json = Column(Text)
+    predicted_class = Column(String(50))
+    confidence = Column(Float)
+    latency = Column(Float) # Float at a fex decimal rounded for more accuracy measurment
+    created_at = Column(DateTime, default=datetime.now())
+
+class RunsCreate(BaseModel):
+    case_id : int
+    prompt_id : int
+    model_used : str
+    prediction_json : str
+    predicted_class : str
+    confidence : float
+    latency : float
+
+    # Validators here
+
+class RunsResponse(BaseModel):
+    id : int
+    case_id : int
+    prompt_id : int
+    model_used : str
+    prediction_json : str
+    predicted_class : str
+    confidence : float
+    latency : float
+    created_at : datetime
+
+class Evaluations(Base):
+    __tablename__ = "evaluations"
+    id = Column(Integer, primary_key=True, index=True)
+    run_id = Column(Integer, ForeignKey(Runs.id), nullable=False)
+    true_label_case = Column(Integer, ForeignKey(Case.id), nullable=False)
+    is_correct = Column(Boolean, nullable=False)
+    error_type = Column(String(64))
+    comments = Column(Text)
+    created_at = Column(DateTime, default=datetime.now())
+
+class EvaluationsCreate(BaseModel):
+    run_id : int
+    true_label_case : int
+    is_correct : bool
+    error_type : str
+    comments : str
+
+    # Validators here
+
+class EvaluationsResponse(BaseModel):
+    id : int
+    run_id : int
+    true_label_case : int
+    is_correct : bool
+    error_type : str
+    comments : str
+    created_at : datetime
 
 Base.metadata.create_all(bind=engine) # This line must always be last in order to be able to create and synchronize db schema
 
